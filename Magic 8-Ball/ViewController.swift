d174174d536ecfb7835808f820670157e2d6b506
sizzzler <53013956+sizzzler@users.noreply.github.com>
2023-01-30 16:50:32 -0800

    Initial Commit

---
+++ Magic 8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8-Ball
+//
+//  Created by Sizi Rios on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
